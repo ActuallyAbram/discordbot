202e6ad931aa899b6b283c2d38041050943324e2
ActuallyAbram <49757984+ActuallyAbram@users.noreply.github.com>
2022-06-24 16:47:50 -0400

    add functionality for login

---
+++ site/backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const passport=require("passport");
+const passportSetup=require("./config/passport-setup");
+const session=require('express-session')
+const port= 4000;
+
+app.use(passport.initialize());
+app.use(session({
+     secret: 'keyboard cat',
+     resave: true,
+     saveUninitialized: true }
+));
+
+
+
+/* Path for auth/discord from browser*/
+app.get('/auth/discord',passport.authenticate('discord',{permissions:8 }));
+app.get('/auth/discord/callback',passport.authenticate('discord',{
+    failureRedirect:'/'
+}), function(req,res){
+    res.redirect('http://localhost:3000/')
+});
+
+app.listen(port, () => console.log('Server is running on port '+ port));
+
